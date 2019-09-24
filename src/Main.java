50799c608e1f6b0fd193a11909bf6bac7789f984
Lise Pedersen <LisePedersen@Lises-MacBook-Air.local>
2019-09-24 09:06:00 +0200

    Lise har hacket kristian haha

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         String myUsername = "DasIsVas1";
-        String myPassword = "Tr4n3v3j63";
+        String myPassword = "1234";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
